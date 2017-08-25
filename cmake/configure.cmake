d40295bca498cfc0e025cb7419a0cc920a836918
Mu Yang <emfomy@gmail.com>
2017-08-25 11:46:52 +0800

    Convert codes to CMake configure files.

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -1,37 +1,46 @@
 # Create configure files
-function(ISVD_CONFIGURE_FN)
+function(ISVD_CONFIGURE_X_FN cfgdir outdir)
   string(REPLACE ";" " " DEFS_STR "${DEFS}")
   file(
     GLOB_RECURSE cfgfiles
-    RELATIVE "${PROJECT_SOURCE_DIR}" "${PROJECT_SOURCE_DIR}/*.in"
+    RELATIVE "${cfgdir}" "${cfgdir}/*\@x\@*.xin"
   )
+  set(X D)
+  set(x d)
+  set(xtype double)
   foreach(cfgfile ${cfgfiles})
-    string(REGEX REPLACE "\\.in$" "" file ${cfgfile})
+    string(REGEX REPLACE "\@x\@" "d" outfile ${cfgfile})
+    string(REGEX REPLACE "\\.xin$" "" outfile ${outfile})
     configure_file(
-      "${PROJECT_SOURCE_DIR}/${cfgfile}"
-      "${PROJECT_BINARY_DIR}/${file}"
+      "${cfgdir}/${cfgfile}"
+      "${outdir}/${outfile}"
       @ONLY
     )
   endforeach()
+  unset(X)
+  unset(x)
+  unset(xtype)
+endfunction()
+
+function(ISVD_CONFIGURE_IN_FN cfgdir outdir)
+  string(REPLACE ";" " " DEFS_STR "${DEFS}")
   file(
     GLOB_RECURSE cfgfiles
-    RELATIVE "${PROJECT_BINARY_DIR}" "${PROJECT_BINARY_DIR}/*.in"
+    RELATIVE "${cfgdir}" "${cfgdir}/*.in"
   )
   foreach(cfgfile ${cfgfiles})
-    string(REGEX REPLACE "\\.in$" "" file ${cfgfile})
+    string(REGEX REPLACE "\\.in$" "" outfile ${cfgfile})
     configure_file(
-      "${PROJECT_BINARY_DIR}/${cfgfile}"
-      "${PROJECT_BINARY_DIR}/${file}"
+      "${cfgdir}/${cfgfile}"
+      "${outdir}/${outfile}"
       @ONLY
     )
   endforeach()
 endfunction()
 
-isvd_configure_fn()
-unset(isvd_configure_fn)
-
-# Set install rule
-install(DIRECTORY "${PROJECT_SOURCE_DIR}/include/" DESTINATION include PATTERN "*.in" EXCLUDE)
-install(DIRECTORY "${PROJECT_BINARY_DIR}/include/" DESTINATION include PATTERN "*.in" EXCLUDE)
-install(DIRECTORY "${PROJECT_SOURCE_DIR}/share/"   DESTINATION share   PATTERN "*.in" EXCLUDE)
-install(DIRECTORY "${PROJECT_BINARY_DIR}/share/"   DESTINATION share   PATTERN "*.in" EXCLUDE)
+isvd_configure_x_fn("${PROJECT_SOURCE_DIR}" "${PROJECT_BINARY_DIR}")
+isvd_configure_x_fn("${PROJECT_BINARY_DIR}" "${PROJECT_BINARY_DIR}")
+isvd_configure_in_fn("${PROJECT_SOURCE_DIR}" "${PROJECT_BINARY_DIR}")
+isvd_configure_in_fn("${PROJECT_BINARY_DIR}" "${PROJECT_BINARY_DIR}")
+unset(isvd_configure_x_fn)
+unset(isvd_configure_in_fn)
