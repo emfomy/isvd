bf0df2f729634704a2cb275d52bea34dfaf32e8f
Mu Yang <emfomy@gmail.com>
2018-10-19 11:54:14 +0800

    Add setup.py.

---
+++ src/pyisvd/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import setuptools
+
+__version__ = '0.0.0'
+
+setuptools.setup(
+	name='isvd',
+	version=__version__,
+	author='Mu Yang',
+	author_email='emfomy@gmail.com',
+	description='Integrated Singular Value Decomposition (iSVD)',
+	url='https://github.com/emfomy/isvd',
+	classifiers=[
+		'Development Status :: 1 - Planning',
+		'Environment :: Console',
+		'Programming Language :: C',
+		'Programming Language :: Python',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: POSIX :: Linux',
+	],
+)
