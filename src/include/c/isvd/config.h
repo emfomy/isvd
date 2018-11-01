d0d2dda8b309af839a7142eb5572a4d6176818f6
Mu Yang <emfomy@gmail.com>
2018-11-01 11:06:32 +0800

    Update version number.

--- src/include/c/isvd/config.h
+++ src/include/c/isvd/config.h
@@ -13,6 +13,9 @@
 #define ISVD_MAJOR_VERSION @ISVD_MAJOR_VERSION@  ///< The major version of iSVD.
 #define ISVD_MINOR_VERSION @ISVD_MINOR_VERSION@  ///< The minor version of iSVD.
 #define ISVD_PATCH_VERSION @ISVD_PATCH_VERSION@  ///< The patch version of iSVD.
-#define ISVD_VERSION       "@ISVD_MAJOR_VERSION@.@ISVD_MINOR_VERSION@.@ISVD_PATCH_VERSION@"  ///< The version of iSVD.
+#define ISVD_TWEAK_VERSION @ISVD_TWEAK_VERSION@  ///< The tweak version of iSVD.
+
+/// The version of iSVD.
+#define ISVD_VERSION "@ISVD_MAJOR_VERSION@.@ISVD_MINOR_VERSION@.@ISVD_PATCH_VERSION@.@ISVD_TWEAK_VERSION@"
 
 #endif  // ISVD_CONFIG_H_
